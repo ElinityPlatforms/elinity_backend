89a1c977d48448fd3a413936d83dc5f32f112399
2100080051 <2100080051ai.ds@gmail.com>
2026-02-13 18:15:42 +0530

    chore: initial clean push of core elinity backend

--- celery_worker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
- 
-celery -A core.celery.celery_app worker --pool=solo --loglevel=info -Q elinity_ai
--- dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .venv/bin/activate
-
-uvicorn dashboard.app:app --host 0.0.0.0 --port 8082 --reload
